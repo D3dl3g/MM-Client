3254892f837aef0a115eb3024c9d1039ee528bf6
D3dl3g <48180011+D3dl3g@users.noreply.github.com>
2024-12-29 22:28:41 +0000

    Update Auto_Pi0_1_2.sh

--- Auto_Pi0_1_2.sh
+++ Auto_Pi0_1_2.sh
@@ -1,2 +1,2 @@
-sudo apt update && sudo apt upgrade -y && sudo apt install xserver-xorg xinit openbox konsole luakit midori xserver-xorg-video-fbdev lightdm lm-sensors --no-install-recommends -y 
+sudo apt update && sudo apt upgrade -y && sudo apt install xserver-xorg xorg xinit openbox konsole luakit midori xserver-xorg-video-fbdev lm-sensors --no-install-recommends -y 
 25min install
